6b60d2e412f3be8f0e8c62af4c34b29af5a79050
Azymis <jgustan@emi.u-bordeaux1.fr>
2016-01-06 16:43:58 +0100

    Ajout des fichiers de base à remplir

---
+++ src/Evaluation.c
@@ -0,0 +1,12 @@
+#include "include/Shell.h"
+#include "include/Evaluation.h"
+#include "include/Commandes_Internes.h"
+
+int
+evaluer_expr(Expression *e)
+{
+    
+  fprintf(stderr,"fonctionnalité non implémentée\n");
+  
+  return 1;
+}
